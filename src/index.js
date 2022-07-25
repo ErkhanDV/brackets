5048516687f25d5f1044f72054f4538bdf98923e
ErkhanDV <erhan.denis@list.ru>
2022-07-25 08:18:40 +0500

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  for (let char of str) {
+    if (stack.length == 0) {
+      stack.push(char);
+    } else {
+      if (bracketsConfig.flat().indexOf(stack[stack.length - 1]) == bracketsConfig.flat().lastIndexOf(char) - 1) {
+      stack.pop();
+      } else {
+        stack.push(char);
+      } 
+    }
+  }
+  return stack.length > 0 ? false : true;
 }
